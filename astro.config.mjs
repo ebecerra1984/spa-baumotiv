fbe00eb2d143ec1bec792ac4823b7b279a6aa85a
Ezequiel Becerra <ebecerra1984@gmail.com>
2024-06-27 19:27:43 -0300

    cambio en config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: "server",
   adapter: vercel()
 });
