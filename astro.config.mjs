764ba3f1e11f7a581ae2ba73f3f5ebeb77fddad3
Ezequiel Becerra <ebecerra1984@gmail.com>
2024-06-27 19:29:40 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
